60fee46676396ca231ea89d720bc723afba925f2
Your Name <oigor8206@gmail.com>
2025-12-02 17:02:06 -0300

    terca aula bigas

--- src/pages/cadastro/Login.jsx
+++ src/pages/cadastro/Login.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import "./simples.css";
+
+
+
 
 const Login = () => {
     const [email, setEmail] = useState("");
